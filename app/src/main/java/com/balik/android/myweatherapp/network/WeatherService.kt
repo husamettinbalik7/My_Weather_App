6d038753bf36291e258f72d086ba2273d4fa2000
Hbalik29 <hbalik29@outlook.com>
2023-07-04 20:23:26 +0300

    addInterceptor

--- app/src/main/java/com/balik/android/myweatherapp/network/WeatherService.kt
+++ app/src/main/java/com/balik/android/myweatherapp/network/WeatherService.kt
@@ -1,7 +1,11 @@
 package com.balik.android.myweatherapp.network
 
+import android.os.Environment
 import com.balik.android.myweatherapp.model.WeatherResponse
 import com.balik.android.myweatherapp.util.Constants
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -23,10 +27,16 @@ interface WeatherService {
     companion object{
 
         fun create() : WeatherService {
+            val httpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
 
             val retrofit = Retrofit.Builder()
                 .baseUrl(Constants.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build()
 
             return retrofit.create(WeatherService::class.java)
